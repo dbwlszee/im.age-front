d685ad1980f9c7cd5efa452932fb1e571946b9fb
Eugene <dbwlszee@gmail.com>
2023-05-02 03:52:28 +0900

    style: 글로벌 스타일 추가
    1. style reset(margin, padding, border을 0으로)
    2. position속성값- body는 relative로, 하위 component들은 absolute로 둘 예정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import styled from "styled-components"
+import GlobalStyle from './styles/GlobalStyle';
 import Nav from "./component/user/Nav";
+
+//style
+styled(Nav)`
+  position: absolute;
+  left: 0px;
+  top: 0px;
+`;
+
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle/>
       <Nav/>
     </div>
   );
---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from "styled-components";
+
+// 전역 style
+const GlobalStyle = createGlobalStyle`
+    * {
+        margin:0;padding:0;border:0;
+    }
+    body{
+        position: relative;
+        width: 100%;
+        margin: 0;
+
+        font-family: 'Nunito', 'Segoe UI', Tahoma, Verdana, sans-serif;
+        background: #f7f7f7;
+    }
+    a{
+        text-decoration: none;
+    }
+    li{
+        list-style: none;
+    }
+`;
+
+export default GlobalStyle;
