656d0b64178fc0e0c08db0b529b0574318f7c4aa
Eugene <dbwlszee@gmail.com>
2023-07-10 03:56:19 +0900

    feat: 통신을 위한 메소드 작성
    1. Axios라이브러리를 통해 Springboot와 HTTP 비동기 통신 수행

--- src/ApiService.js
+++ src/ApiService.js
@@ -1,14 +1,16 @@
 import axios from 'axios';
 
-const PRODUCT_API_BASE_URL = "http://localhost:9090/api/product";
+const PRODUCT_API_BASE_URL = "http://localhost:9090/api";
 
 class ApiService {
+    // 전체 Product리스트
     fetchProducts(){
-        return axios.get(PRODUCT_API_BASE_URL);
+        return axios.get(PRODUCT_API_BASE_URL + "/product");
     }
 
+    // 제품 상세정보
     fetchProductsByID(productID) {
-        return axios.get(PRODUCT_API_BASE_URL + '/' + productID);
+        return axios.get(PRODUCT_API_BASE_URL + '/' + productID + '/detail');
     }
 }
 
