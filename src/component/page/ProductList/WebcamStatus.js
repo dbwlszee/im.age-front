534daf6415ae149ea4e7aca90247b4244ce69dbf
Eugene <dbwlszee@gmail.com>
2023-07-26 03:51:32 +0900

    fix: 로컬 웹캠 처리
    1. 서버에서 로컬 웹캠을 볼수 없는 문제를 해결하기 위함

--- src/component/page/ProductList/WebcamStatus.js
+++ src/component/page/ProductList/WebcamStatus.js
@@ -2,65 +2,87 @@ import React, { useEffect, useRef, useState } from "react";
 import io from 'socket.io-client';
 import Webcam from "react-webcam";
 import Peer from 'peerjs';
-// import { useContext } from "react";
-// import { WebcamContext } from "../../context/WebcamContext";
 import ApiService from "../../../ApiService";
 
 
 const WebcamStatus = ({ onDataChange }) => {
     const socketRef = useRef();
     const webcamRef = useRef(null);
-    const peerRef = useRef(null);
     const [tempProduct, setTempProduct] = useState(null);
-    // const [imgUrl, setimgUrl] = useState();
-    // const { displayCam, setDisplayCam } = useContext(WebcamContext);
 
     // Lifting State Up을 위함.
     const sendData = () => {
         onDataChange(tempProduct);
     };
+    
+    const initPeer = async () => {
+        
+        // PeerJS 서버의 호스트 주소와 포트 번호 설정
+        const peerHost = process.env.REACT_APP_PEER_HOST;
+        const peerPort = process.env.REACT_APP_PEER_PORT;
+
+        // PeerJS 설정
+        const peer = new Peer('tempid', {
+          host: peerHost, // PeerJS 서버 호스트 주소
+          port: peerPort, // PeerJS 서버 포트 번호
+          path: '/myapp' // 선택적 경로 (옵션)
+        });
+  
+        try {
+          // getUserMedia로 웹캠 스트림 가져오기
+          const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+          // 웹캠 스트림을 비디오 요소에 연결하여 웹캠 화면 표시
+          webcamRef.current.srcObject = stream;
+        } catch (error) {
+          console.error('Error accessing the webcam:', error);
+        }
+  
+        // 에러 처리
+        peer.on('error', (error) => {
+          console.error('PeerJS Error:', error);
+        });
+    };
+
 
     useEffect(()=>{
-        // 웹소켓 서버 연결
-        socketRef.current = io.connect('http://localhost:5000');
-        const peer = new Peer();
+            initPeer();
 
-        peerRef.current = peer;
+            // 소켓 연결 설정
+            const socket = io('http://localhost:5000');
+            socketRef.current = socket;
 
-        // PeerJS 서버에 연결되었을 때 실행
-        peer.on('open', (id) => {
-            console.log('Connected with ID:', id);
-        });
-        
-        const interval = setInterval(() => {
-            // 현재 프레임 가져오기
-            const currentFrame = webcamRef.current.getScreenshot();
+            // 1초마다 현재 프레임을 서버로 전송
+            const interval = setInterval(() => {
+                // 현재 프레임 가져오기
+                const currentFrame = webcamRef.current.getScreenshot();
 
-            // currentFrame이 존재할 때 플라스크로 이미지 전송
-            if(currentFrame) {
-                socketRef.current.emit('webcam_data', { image: currentFrame.substr(23,) });
-            }
-        }, 1000); // 1초마다 현재 프레임 전송
+                // currentFrame이 존재할 때 플라스크로 이미지 전송
+                if (currentFrame) {
+                    socket.emit('webcam_data', { image: currentFrame.substr(23,) });
+                }
+            }, 1000);
 
 
-        //flask에서 db가 업데이트 되었다는 소식을 받으면 상품리스트를 springboot에서 받아온다.
-        socketRef.current.on('update', (data) => {
-            ApiService.fetchProducts()
-            .then( res => {
-                setTempProduct(res.data);
-            })
-            .catch(err => {
-                console.log('reloadRequest Error.', err);
+            //flask에서 db가 업데이트 되었다는 소식을 받으면 상품리스트를 springboot에서 받아온다.
+            socketRef.current.on('update', (data) => {
+                ApiService.fetchProducts()
+                .then( res => {
+                    setTempProduct(res.data);
+                })
+                .catch(err => {
+                    console.log('reloadRequest Error.', err);
+                });
             });
-        });
 
 
-        // 컴포넌트 언마운트 됐을 때 웹소켓 연결 종료
-        return() => {
-            clearInterval(interval);
-            socketRef.current.disconnect();
-            peerRef.current.destroy();
-        };
+            // 컴포넌트 언마운트 됐을 때 웹소켓 연결 종료
+            return() => {
+                if (socketRef.current) {
+                    socketRef.current.disconnect();
+                }
+                clearInterval(interval);
+            };
+            
     }, []);
 
     // tempProduct 상태 변경 시 sendData 호출
@@ -71,10 +93,11 @@ const WebcamStatus = ({ onDataChange }) => {
 
     return (
         <div>
+            <p>webcam</p>
             <Webcam
                 audio={false}
-                height={1080}
-                width={1920}
+                height={300}
+                width={400}
                 ref={webcamRef}
                 screenshotFormat="image/jpeg"
                 style={{display: "block"}}
